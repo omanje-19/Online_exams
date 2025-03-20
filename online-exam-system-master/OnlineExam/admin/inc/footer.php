e01e8df226ed47b310b7730e36eee45946324002
omanje-19 <omanjejanet19@gmail.com>
2025-03-20 08:40:16 +0300

    name changes

--- online-exam-system-master/OnlineExam/admin/inc/footer.php
+++ online-exam-system-master/OnlineExam/admin/inc/footer.php
@@ -1,6 +1,6 @@
  </section>
 <section class="footeroption">
-		<h2><?php echo "Dept. of Computer Science & Engineering, JUST"; ?></h2>
+		<h2><?php echo "Dept. of Computer Science & Engineering, MAJISMART"; ?></h2>
 	</section>
 </div>
 </body>
